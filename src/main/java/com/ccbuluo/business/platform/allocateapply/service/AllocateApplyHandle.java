efe1f80f468dc3dd23f10bc3cd9c8da5020b3fcf
wei'j'b <281803149@qq.com>
2018-08-13 17:51:40 +0800

    申请提交

---
+++ src/main/java/com/ccbuluo/business/platform/allocateapply/service/AllocateApplyHandle.java
@@ -0,0 +1,11 @@
+package com.ccbuluo.business.platform.allocateapply.service;
+
+/**
+ * ${todo}
+ *
+ * @author weijb
+ * @version v1.0.0
+ * @date 2018-08-13 17:48:56
+ */
+public interface AllocateApplyHandle {
+}
