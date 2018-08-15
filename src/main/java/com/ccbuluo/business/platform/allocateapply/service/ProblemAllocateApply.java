11b6c03ce667d00609452222890c16eac7757577
wei'j'b <281803149@qq.com>
2018-08-15 21:24:32 +0800

    出入库计划加机构编号，及问题件查询列表

---
+++ src/main/java/com/ccbuluo/business/platform/allocateapply/service/ProblemAllocateApply.java
@@ -0,0 +1,42 @@
+package com.ccbuluo.business.platform.allocateapply.service;
+
+
+import com.ccbuluo.business.platform.allocateapply.dto.ProblemAllocateapplyDetailDTO;
+import com.ccbuluo.business.platform.stockdetail.dto.StockBizStockDetailDTO;
+import com.ccbuluo.db.Page;
+import com.ccbuluo.merchandiseintf.carparts.parts.dto.BasicCarpartsProductDTO;
+
+import java.util.List;
+
+/**
+ * 功能描述（1）
+ *
+ * @author weijb
+ * @version v1.0.0
+ * @date 创建时间（2）
+ */
+public interface ProblemAllocateApply {
+
+    /**
+     * 问题件申请列表
+     * @param applyType 申请类型
+     * @param applyStatus 申请状态
+     * @param applyNo 申请单号
+     * @param offset 起始数
+     * @param pageSize 每页数量
+     * @author weijb
+     * @date 2018-08-14 21:59:51
+     */
+    Page<ProblemAllocateapplyDetailDTO> queryProblemApplyList(String applyType, String applyStatus, String applyNo, Integer offset, Integer pageSize);
+    /**
+     * 问题件处理列表
+     * @param applyType 申请类型
+     * @param applyStatus 申请状态
+     * @param applyNo 申请单号
+     * @param offset 起始数
+     * @param pageSize 每页数量
+     * @author weijb
+     * @date 2018-08-15 18:51:51
+     */
+    Page<ProblemAllocateapplyDetailDTO> queryProblemHandleList(String applyType, String applyStatus, String applyNo, Integer offset, Integer pageSize);
+}
