0ad5eb127d0cf5c13bf2c4a4af6ae7c3a6df16b6
wei'j'b <281803149@qq.com>
2018-08-17 14:21:18 +0800

    机构编号的保存

--- src/main/java/com/ccbuluo/business/platform/outstockplan/dao/BizOutstockplanDetailDao.java
+++ src/main/java/com/ccbuluo/business/platform/outstockplan/dao/BizOutstockplanDetailDao.java
@@ -120,12 +120,12 @@ public class BizOutstockplanDetailDao extends BaseDao<BizOutstockplanDetail> {
                 .append("product_no,product_type,trade_no,supplier_no,apply_detail_id,")
                 .append("cost_price,sales_price,out_repository_no,plan_outstocknum,")
                 .append("actual_outstocknum,plan_status,complete_time,creator,create_time,")
-                .append("operator,operate_time,delete_flag,remark,product_categoryname,product_unit,product_name,stock_type")
+                .append("operator,operate_time,delete_flag,remark,product_categoryname,product_unit,product_name,stock_type,out_orgno")
                 .append(" ) VALUES (  :outstockType, :stockId, :productNo, :productType,")
                 .append(" :tradeNo, :supplierNo, :applyDetailId, :costPrice, :salesPrice,")
                 .append(" :outRepositoryNo, :planOutstocknum, :actualOutstocknum, :planStatus,")
                 .append(" :completeTime, :creator, :createTime, :operator, :operateTime,")
-                .append(" :deleteFlag, :remark, :productCategoryname, :productUnit, :productName, :stockType )");
+                .append(" :deleteFlag, :remark, :productCategoryname, :productUnit, :productName, :stockType, :outOrgno )");
         List<Long> longs = super.batchInsertForListBean(sql.toString(), list);
         return longs;
     }
