063115b58c69e901ae258ee32af97a39b763916d
wei'j'b <281803149@qq.com>
2018-08-20 18:33:57 +0800

    入库之后回调事件

--- src/main/java/com/ccbuluo/business/platform/allocateapply/service/applyhandle/PurchaseApplyHandleStrategy.java
+++ src/main/java/com/ccbuluo/business/platform/allocateapply/service/applyhandle/PurchaseApplyHandleStrategy.java
@@ -2,6 +2,7 @@ package com.ccbuluo.business.platform.allocateapply.service.applyhandle;
 
 import com.auth0.jwt.internal.org.apache.commons.lang3.tuple.Pair;
 import com.ccbuluo.business.entity.*;
+import com.ccbuluo.business.platform.allocateapply.dao.BizAllocateApplyDao;
 import com.ccbuluo.business.platform.allocateapply.dao.BizAllocateapplyDetailDao;
 import com.ccbuluo.business.platform.allocateapply.dto.AllocateapplyDetailBO;
 import com.ccbuluo.business.platform.inputstockplan.dao.BizInstockplanDetailDao;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -41,6 +43,8 @@ public class PurchaseApplyHandleStrategy extends DefaultApplyHandleStrategy {
     private BizOutstockplanDetailDao bizOutstockplanDetailDao;
     @Resource
     private BizStockDetailDao bizStockDetailDao;
+    @Resource
+    BizAllocateApplyDao bizAllocateApplyDao;
 
     Logger logger = LoggerFactory.getLogger(getClass());
     /**
@@ -81,7 +85,29 @@ public class PurchaseApplyHandleStrategy extends DefaultApplyHandleStrategy {
      * @author weijb
      * @date 2018-08-11 13:35:41
      */
+    @Transactional(rollbackFor = Exception.class)
+    @Override
     public StatusDto cancelApply(String applyNo){
+        try {
+            // 根据申请单编号查询订单占用库存关系表
+            List<RelOrdstockOccupy> list = bizAllocateTradeorderDao.getRelOrdstockOccupyByApplyNo(applyNo);
+            //根据订单占用库存关系构建库存list
+            List<BizStockDetail> stockDetails = buildBizStockDetail(list);
+            // 还原被占用的库存
+            int flag = bizStockDetailDao.batchUpdateStockDetil(stockDetails);
+            if(flag == 0){// 更新失败
+                throw new CommonException("0", "还原占用库存失败！");
+            }
+            // 删除订单
+            bizAllocateTradeorderDao.deleteAllocateTradeorderByApplyNo(applyNo);
+            // 删除入库计划
+            bizInstockplanDetailDao.batchInsertInstockplanDetail(applyNo);
+            //更新申请单状态(已撤销)
+            bizAllocateApplyDao.updateApplyOrderStatus(applyNo, BizAllocateApply.ApplyStatusEnum.CANCEL.name());
+        } catch (Exception e) {
+            logger.error("撤销失败！", e);
+            throw e;
+        }
         return StatusDto.buildSuccessStatusDto("申请撤销成功！");
     }
 
@@ -92,7 +118,30 @@ public class PurchaseApplyHandleStrategy extends DefaultApplyHandleStrategy {
      */
     @Override
     public StatusDto platformInstockCallback(BizAllocateApply ba){
-        return platformInstockCallback(ba);
+        return super.platformInstockCallback(ba);
     }
 
-}
+    /**
+     *  构建出库和入库计划并保存
+     * @param details 申请单详情
+     * @param stockDetails 库存详情列表
+     * @param applyTypeEnum 申请类型枚举
+     * @author weijb
+     * @date 2018-08-11 13:35:41
+     */
+    @Override
+    public Pair<List<BizOutstockplanDetail>, List<BizInstockplanDetail>> buildOutAndInstockplanDetail(List<AllocateapplyDetailBO> details, List<BizStockDetail> stockDetails, BizAllocateApply.AllocateApplyTypeEnum applyTypeEnum, List<RelOrdstockOccupy> relOrdstockOccupies){
+        List<BizOutstockplanDetail> outList = new ArrayList<BizOutstockplanDetail>();
+        List<BizInstockplanDetail> inList = new ArrayList<BizInstockplanDetail>();
+        //  给入库之后回调出库计划用
+        if(null != stockDetails && null != relOrdstockOccupies){
+            outstockplanPlatform(outList,relOrdstockOccupies,stockDetails,details, BizAllocateApply.AllocateApplyTypeEnum.PLATFORMALLOCATE.toString());
+        }
+        // 平台入库计划
+        instockplanPlatform(inList,details, BizAllocateApply.AllocateApplyTypeEnum.PURCHASE.toString());
+        // 买入方入库计划
+        instockplanPurchaser(inList,details, BizAllocateApply.AllocateApplyTypeEnum.PURCHASE.toString());
+        return Pair.of(outList, inList);
+    }
+
+    }
