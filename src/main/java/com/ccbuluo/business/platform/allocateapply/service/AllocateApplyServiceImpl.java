9a9cb00b797756d86989d17fb2590c760d822e6e
wei'j'b <281803149@qq.com>
2018-08-21 18:25:28 +0800

    平台调拨买方入库计划代码改造

--- src/main/java/com/ccbuluo/business/platform/allocateapply/service/AllocateApplyServiceImpl.java
+++ src/main/java/com/ccbuluo/business/platform/allocateapply/service/AllocateApplyServiceImpl.java
@@ -493,7 +493,6 @@ public class AllocateApplyServiceImpl implements AllocateApplyService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public StatusDto cancelApply(String applyNo){
-        // 更改申请单状态 TODO
         return applyHandleContext.cancelApply(applyNo);
     }
 }
