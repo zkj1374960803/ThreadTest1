fd4ffd81f073a727de28fd5ee1eb0a8a587719bc
wei'j'b <281803149@qq.com>
2018-09-03 11:40:23 +0800

    问题件列表获取成本价

--- src/main/java/com/ccbuluo/business/platform/stockdetail/dao/ProblemStockDetailDao.java
+++ src/main/java/com/ccbuluo/business/platform/stockdetail/dao/ProblemStockDetailDao.java
@@ -158,4 +158,26 @@ public class ProblemStockDetailDao extends BaseDao<BizStockDetail> {
         sql.append(" GROUP BY t1.id  ORDER BY t1.create_time DESC");
         return super.queryListBean(StockDetailDTO.class, sql.toString(), param);
     }
+
+    /**
+     *  根据申请单和组织机构获取商品详情
+     * @param
+     * @exception
+     * @return
+     * @author weijb
+     * @date 2018-09-03 10:22:11
+     */
+    public List<StockDetailDTO> queryStockDetailListByAppNo(String orgCode, String applyNo){
+        Map<String, Object> param = Maps.newHashMap();
+        param.put("deleteFlag", Constants.DELETE_FLAG_NORMAL);
+        param.put("applyNo", applyNo);
+        param.put("orgCode", orgCode);
+        StringBuilder sql = new StringBuilder();
+
+        sql.append("SELECT id,trade_no,product_no,cost_price")
+                .append(" FROM biz_stock_detail ")
+                .append(" WHERE delete_flag = :deleteFlag and trade_no = :applyNo and org_no= :orgCode");
+        sql.append(" GROUP BY id ");
+        return super.queryListBean(StockDetailDTO.class, sql.toString(), param);
+    }
 }
