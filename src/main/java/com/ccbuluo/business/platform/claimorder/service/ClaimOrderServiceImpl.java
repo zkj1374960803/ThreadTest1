c0de95ed3556f5867d814c5ddefdb491f1c7cacd
zhangkangjian <zhangkj@ccbuluo.com>
2018-09-08 10:49:25 +0800

    开发零配件价格

--- src/main/java/com/ccbuluo/business/platform/claimorder/service/ClaimOrderServiceImpl.java
+++ src/main/java/com/ccbuluo/business/platform/claimorder/service/ClaimOrderServiceImpl.java
@@ -7,6 +7,16 @@ package com.ccbuluo.business.platform.claimorder.service;
 public class ClaimOrderServiceImpl implements ClaimOrderService{
 
 
+    /**
+     * 生成索赔单
+     *
+     * @return
+     * @throws
+     * @author zhangkangjian
+     * @date 2018-09-08 10:43:18
+     */
+    @Override
+    public void generateClaimForm() {
 
-
+    }
 }
