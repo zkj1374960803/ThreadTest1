8ee09c60d2fb1e6d851c3b296340e619e153fd98
zhangkangjian <zhangkj@ccbuluo.com>
2018-09-15 19:42:55 +0800

    申请模块调整

--- src/main/java/com/ccbuluo/business/platform/inputstockplan/dao/BizInstockplanDetailDao.java
+++ src/main/java/com/ccbuluo/business/platform/inputstockplan/dao/BizInstockplanDetailDao.java
@@ -296,7 +296,7 @@ public class BizInstockplanDetailDao extends BaseDao<BizInstockplanDetail> {
         StringBuilder sql = new StringBuilder();
         sql.append("SELECT a.id,a.instock_type,a.product_no,a.product_unit,a.product_name,a.product_type,a.product_categoryname,a.seller_orgno AS 'outOrgno',")
             .append("a.trade_no,a.supplier_no,a.instock_repository_no,a.cost_price,b.supplier_name,")
-            .append("IFNULL(a.plan_instocknum,0) AS planOutstocknum,IFNULL(a.actual_instocknum,0) AS actualInstocknum,a.complete_status,a.complete_time")
+            .append("IFNULL(a.plan_instocknum,0) AS 'planOutstocknum',IFNULL(a.actual_instocknum,0) AS actualInstocknum,a.complete_status,a.complete_time")
             .append(" FROM biz_instockplan_detail a left join biz_service_supplier b on a.supplier_no = b.supplier_code")
             .append(" WHERE 1 = 1 ");
         if(StringUtils.isNotBlank(applyNo)){
