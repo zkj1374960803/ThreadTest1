172cd594938ce9da97665b5c75a36f88e9f22f80
zhangkangjian <zhangkj@ccbuluo.com>
2018-09-29 11:37:15 +0800

    查询零配件价格列表bug修改

--- src/main/java/com/ccbuluo/business/platform/carparts/service/CarpartsProductPriceServiceImpl.java
+++ src/main/java/com/ccbuluo/business/platform/carparts/service/CarpartsProductPriceServiceImpl.java
@@ -16,6 +16,7 @@ import com.ccbuluo.merchandiseintf.carparts.parts.dto.BasicCarpartsProductDTO;
 import com.ccbuluo.merchandiseintf.carparts.parts.dto.QueryCarpartsProductDTO;
 import com.ccbuluo.merchandiseintf.carparts.parts.service.CarpartsProductService;
 import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -82,7 +83,6 @@ public class CarpartsProductPriceServiceImpl implements CarpartsProductPriceServ
 
     /**
      * 查询维修单的零配件列表
-     *
      * @param queryCarpartsProductDTO 查询条件
      * @return StatusDto<Page < BasicCarpartsProductDTO>> 查询分页信息
      * @author zhangkangjian
@@ -113,26 +113,31 @@ public class CarpartsProductPriceServiceImpl implements CarpartsProductPriceServ
 
     /**
      * 填充销售价格和车型名称
-     * @param queryCarpartsProductDTO
-     * @param relProductPrice
-     * @exception
-     * @return
+     * @param queryCarpartsProductDTO 零配件
+     * @param relProductPriceList 价格
+     * @return StatusDto<Page<BasicCarpartsProductDTO>> 分页零配件
      * @author zhangkangjian
      * @date 2018-09-28 11:27:10
      */
-    private StatusDto<Page<BasicCarpartsProductDTO>> getPageStatusDto(QueryCarpartsProductDTO queryCarpartsProductDTO, List<RelProductPrice> relProductPrice) {
+    private StatusDto<Page<BasicCarpartsProductDTO>> getPageStatusDto(QueryCarpartsProductDTO queryCarpartsProductDTO, List<RelProductPrice> relProductPriceList) {
         StatusDtoThriftPage<BasicCarpartsProductDTO> basicCarpartsProductDTO =
             carpartsProductService.queryCarpartsProductListByPriceType(queryCarpartsProductDTO);
         StatusDto<Page<BasicCarpartsProductDTO>> basicCarpartsProductDTOResolve = StatusDtoThriftUtils.resolve(basicCarpartsProductDTO, BasicCarpartsProductDTO.class);
         List<BasicCarpartsProductDTO> rows = basicCarpartsProductDTOResolve.getData().getRows();
 
-        Optional.ofNullable(relProductPrice).ifPresent(a ->{
+        Optional.ofNullable(relProductPriceList).ifPresent(a ->{
             Map<String, RelProductPrice> relProductPriceMap = a.stream().collect(Collectors.toMap(RelProductPrice::getProductNo, b -> b,(k1, k2)->k1));
             if(rows != null && rows.size() > 0){
                 rows.forEach(item ->{
-                    RelProductPrice relProductPrice1 = relProductPriceMap.get(item.getCarpartsCode());
-                    if(relProductPrice1 != null){
-                        item.setCarpartsPrice(String.valueOf(relProductPrice1.getSuggestedPrice()));
+                    String categoryCodePath = item.getCategoryCodePath();
+                    if(StringUtils.isNotBlank(categoryCodePath)){
+                        int i = categoryCodePath.indexOf("-") + 1;
+                        String substring = categoryCodePath.substring(i, categoryCodePath.length());
+                        item.setCategoryCodePath(substring);
+                    }
+                    RelProductPrice relProductPrice = relProductPriceMap.get(item.getCarpartsCode());
+                    if(relProductPrice != null){
+                        item.setCarpartsPrice(String.valueOf(relProductPrice.getSuggestedPrice()));
                     }
                 });
             }
